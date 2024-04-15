f5a33ef75f875635331daee1b1f2fb80aa2bff79
Kaan Genis <kaang18@gmail.com>
2024-04-15 14:33:01 +0300

    Initial Commit

---
+++ SwiftToDo/SwiftToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftToDoApp.swift
+//  SwiftToDo
+//
+//  Created by Kaan Geniş on 15.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
